17e10be40d22b15db36746dea5b249b02d9699ca
djszal <szalank.dj@gmail.com>
2022-12-15 15:12:11 -0500

    finished part 1

--- db/index.js
+++ db/index.js
@@ -2,6 +2,37 @@ const { Client } = require("pg");
 
 const client = new Client("postgres://localhost:5432/juicebox");
 
+async function getAllUsers() {
+  const { rows } = await client.query(
+    `SELECT id, username
+    FROM users;
+    `
+  );
+  // console.log("rows: ", rows);
+  return rows;
+}
+
+async function createUser({ username, password }) {
+  try {
+    const { rows } = await client.query(
+      `
+    INSERT INTO users(username, password) 
+    VALUES ($1, $2) 
+    ON CONFLICT (username) DO NOTHING 
+    RETURNING *; 
+    `,
+      [username, password]
+    );
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
+
 module.exports = {
   client,
+  getAllUsers,
+  createUser,
 };
+
+//In general, in our db/index.js file we should provide the utility functions that the rest of our application will use. We will call them from the seed file, but also from our main application file.
--- db/seed.js
+++ db/seed.js
@@ -1,21 +1,92 @@
 // grab our client with destructuring from the export in index.js
-const { client } = require("./index");
+const { client, getAllUsers, createUser } = require("./index");
+
+async function dropTables() {
+  try {
+    console.log("Starting to drop tables...");
+    await client.query(`
+    DROP TABLE IF EXISTS users;
+    `);
+    console.log("Finished dropping tables!");
+  } catch (error) {
+    console.error("Error in dropTables: ", error);
+  }
+}
+
+async function createTables() {
+  try {
+    console.log("Starting to build tables...");
+    await client.query(`
+    CREATE TABLE users (
+      id SERIAL PRIMARY KEY, 
+      username varchar(255) UNIQUE NOT NULL, 
+      password varchar(255) NOT NULL
+    );
+    `);
+    console.log("Finished building tables!");
+  } catch (error) {
+    console.error("Error in createTables: ", error);
+  }
+}
+
+async function createInitialUsers() {
+  try {
+    console.log("Starting to create users...");
+    const albert = await createUser({
+      username: "albert",
+      password: "bertie99",
+    });
+    const sandra = await createUser({
+      username: "sandra",
+      password: "2sandy4me",
+    });
+    const glamgal = await createUser({
+      username: "glamgal",
+      password: "bsoglam",
+    });
+    console.log(albert);
+    console.log(albertTwo);
+    console.log("Finished creating users!");
+  } catch (error) {
+    console.error("Error createInitialUsers!", error);
+  }
+}
+
+async function rebuildDB() {
+  try {
+    client.connect();
+
+    await dropTables();
+    await createTables();
+    await createInitialUsers();
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 async function testDB() {
   try {
+    console.log("Starting to test database...");
     // connect the client to the database
-    client.connect();
+    // client.connect();
 
     // queries are promises, so we can await them
-    const { rows } = await client.query(`SELECT * FROM users;`);
+    // const { rows } = await client.query(`SELECT * FROM users;`);
+    const users = await getAllUsers();
 
-    console.log(rows);
+    console.log("getAllUsers:", users);
+    console.log("Finished database tests!");
   } catch (error) {
-    console.error(error);
+    console.error("Error testing database!", error);
     // it's important to close out the client connection
   } finally {
     client.end();
   }
 }
 
-testDB();
+// testDB();
+
+rebuildDB()
+  .then(testDB)
+  .catch(console.error)
+  .finally(() => client.end());
